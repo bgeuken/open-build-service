eba95f39a9d05bf22a8910c3bc54e4665f2bf719
Björn Geuken <bgeuken@suse.de>
2018-11-04 16:19:57 +0100

    wip

--- src/api/app/models/staging_project.rb
+++ src/api/app/models/staging_project.rb
@@ -1,3 +1,4 @@
+# The staging project is the place where we collect a selection of packages and test, build and review them.
 class StagingProject < Project
   # TODO: Make sure that we aren't fetching those requests over and over again when using this association
   has_many :staged_requests, class_name: 'BsRequest', foreign_key: :staging_project_id
@@ -33,48 +34,6 @@ class StagingProject < Project
   def broken_packages
     get_buildinfo if @broken_packages.nil?
     @broken_packages
-  end
-
-  # TODO: Move to lib
-  def get_buildinfo
-    buildresult = Buildresult.find_hashed(project: name, view: 'summary')
-
-    @broken_packages = []
-    @building_repositories = []
-
-    buildresult.elements('result') do |result|
-      building = ['published', 'unpublished'].exclude?(result['state']) || result['dirty'] == 'true'
-
-      result.elements('status') do |status|
-        code = status.get('code')
-
-        if code.in?(['broken', 'failed']) || (code == 'unresolvable' && !building)
-          @broken_packages << { package:    status['package'],
-                                project:    name,
-                                state:      code,
-                                details:    status['details'],
-                                repository: result['repository'],
-                                arch:       result['arch'] }.with_indifferent_access
-        end
-      end
-
-      if building
-        current_repo = result.slice('repository', 'arch', 'code', 'state', 'dirty')
-        current_repo[:tobuild] = 0
-        current_repo[:final] = 0
-        result.get('summary').elements('statuscount') do |status_count|
-          if status_count['code'].in?(['excluded', 'broken', 'failed', 'unresolvable', 'succeeded', 'excluded', 'disabled'])
-            current_repo[:final] += status_count['count'].to_i
-          else
-            current_repo[:tobuild] += status_count['count'].to_i
-          end
-        end
-
-        @building_repositories << current_repo
-      end
-    end
-
-    @broken_packages.reject! { |p| p['state'] == 'unresolvable' } if @building_repositories.present?
   end
 
   # TODO: This should be an association to the review model
@@ -130,5 +89,53 @@ class StagingProject < Project
     end
 
     @state
+  end
+
+  private
+
+  # TODO: Move to lib
+  def get_buildinfo
+    # TODO: Clarify whether we really want to get the build result of the staging project and not the
+    #       source projects of the requests.
+    buildresult = Buildresult.find_hashed(project: name, view: 'summary')
+
+    # data of each pkg/repo/arch combo with state broken or failed
+    @broken_packages = []
+    # data and count of all repos that are currently building
+    @building_repositories = []
+
+    buildresult.elements('result') do |result|
+      building = ['published', 'unpublished'].exclude?(result['state']) || result['dirty'] == 'true'
+
+      result.elements('status') do |status|
+        code = status.get('code')
+
+        if code.in?(['broken', 'failed']) || (code == 'unresolvable' && !building)
+          @broken_packages << { package:    status['package'],
+                                project:    name,
+                                state:      code,
+                                details:    status['details'],
+                                repository: result['repository'],
+                                arch:       result['arch'] }.with_indifferent_access
+        end
+      end
+
+      if building
+        current_repo = result.slice('repository', 'arch', 'code', 'state', 'dirty')
+        current_repo[:tobuild] = 0
+        current_repo[:final] = 0
+        result.get('summary').elements('statuscount') do |status_count|
+          if status_count['code'].in?(['excluded', 'broken', 'failed', 'unresolvable', 'succeeded', 'excluded', 'disabled'])
+            current_repo[:final] += status_count['count'].to_i
+          else
+            current_repo[:tobuild] += status_count['count'].to_i
+          end
+        end
+
+        @building_repositories << current_repo
+      end
+    end
+
+    @broken_packages.reject! { |p| p['state'] == 'unresolvable' } if @building_repositories.present?
   end
 end
