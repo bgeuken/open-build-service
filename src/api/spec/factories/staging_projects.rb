e46d16809a33540b01e6fb9530edc46eb496d21e
Dany Marcoux <danymarcoux@gmail.com>
2018-11-05 17:37:11 +0100

    Fix broken factory

--- src/api/spec/factories/staging_projects.rb
+++ src/api/spec/factories/staging_projects.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :staging_project, class: 'StagingProject', parent:  do
+  factory :staging_project, class: 'StagingProject', parent: :project do
     sequence(:name, [*'A'..'Z'].cycle) { |letter| "#{staging_workflow.project.name}:Staging:#{letter}" }
   end
 end
