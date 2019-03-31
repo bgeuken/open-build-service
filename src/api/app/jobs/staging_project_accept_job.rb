64baa9d2f333849f40f91a9a70019be80241cc43
Björn Geuken <bgeuken@suse.de>
2019-04-01 00:47:25 +0200

    Add API endpoint for merging staging projects

---
+++ src/api/app/jobs/staging_project_accept_job.rb
@@ -0,0 +1,10 @@
+class StagingProjectAcceptJob < ApplicationJob
+  queue_as :quick
+
+  def perform(project_id, user_login)
+    current_user_before = User.current
+    User.current = User.find_by(login: user_login)
+    Project.find(project_id).accept_staged_requests
+    User.current = current_user_before
+  end
+end
